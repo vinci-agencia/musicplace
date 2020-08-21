cf497bc0581f24db7d79b0d9c4c6431cfa9df4fe
breno <breno@agenciavinci.com.br>
2020-08-21 11:24:55 -0300

    alteracao da pagina isntruments

--- Javascript.js
+++ Javascript.js
@@ -32,3 +32,14 @@ $('#price-max').change(function(event) {
         $('#price-max').val(minValue);
     }
 });
+$(document).ready(function() {
+    $('#list').click(function(event) {
+        event.preventDefault();
+        $('#products .item').addClass('list-group-item');
+    });
+    $('#grid').click(function(event) {
+        event.preventDefault();
+        $('#products .item').removeClass('list-group-item');
+        $('#products .item').addClass('grid-group-item');
+    });
+});
